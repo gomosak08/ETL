5ad3bbe5f2de9c55df763036e07165626c50fdca
Jorge Vázquez Fuerte <80866685+JorgeVF01@users.noreply.github.com>
2022-04-06 10:33:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ const CSVToJSON = require("csvtojson");
 const JSONToCSV = require("json2csv").parse; 
 const FileSysten = require("fs");
 const { fileURLToPath } = require("url");
+const convert = require('xml-js');
+const XLSX = require("xlsx");
 
-CSVToJSON().fromFile("./source.csv").then(source => {
-    console.log(source);
-    source.push({
-        "sku":"54632",
-        "title":"Fortnite",
-        "hardware":"nintendo switch",
-        "price": "00.00"
+function mod_file(dict){
+    for( const data in dict){
+        const dato = dict[data];
+        
+        if (!isNaN(dato)){
+            dict[data] = parseFloat(dato)+5;
+        } else{
+            dict[data] = dato.split('').reverse().join('');
 
+        }
+    }
+}
+
+
+
+CSVToJSON().fromFile("./dataset.xlsx").then(source => {
+    console.log('Source file: \n',source);
+    source.forEach(line => {
+        mod_file(line);
     });
+    console.log('Modified file: \n',source);
+
     const csv = JSONToCSV(source);
+    console.log(csv);
     FileSysten.writeFileSync("./destination.csv",csv);
 
 });
+
+console.log(parseInt('hola'));
